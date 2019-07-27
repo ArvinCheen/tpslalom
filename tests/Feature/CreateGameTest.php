155b4d6484b79ee7fe9fdc4d93f793673ea6d814
陳俊良 <jafar@ip-192-168-0-63.ap-northeast-1.compute.internal>
2019-07-27 16:21:15 +0800

    new v1

---
+++ tests/Feature/CreateGameTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CreateGameTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testView()
+    {
+//        $response = $this->get('/createGame');
+//
+//        $response->assertStatus(200);
+    }
+}
