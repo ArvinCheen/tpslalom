579752678077f510f0802aefceb9c6e8d9398d41
jafar <jafartago@gmail.com>
2019-10-26 20:37:08 +0800

    hide

--- resources/views/gameInfo/appearance.blade.php
+++ resources/views/gameInfo/appearance.blade.php
@@ -33,7 +33,7 @@
                     <table class="table table-striped table-bordered table-advance table-hover">
                         <thead>
                         <tr>
-                            <th class="text-center"> 順序 </th>
+{{--                            <th class="text-center"> 順序 </th>--}}
                             <th class="text-center"> 編號 </th>
                             <th class="text-center"> 選手 </th>
                             <th class="text-center"> 編號 </th>
@@ -43,56 +43,56 @@
                         <tbody>
                         @if ($isView)
                             <tr>
-                                <th class="text-center"> 1 </th>
+{{--                                <th class="text-center"> 1 </th>--}}
                                 <th class="text-center"> 180 </th>
                                 <th class="text-center"> 鐘晨恩 </th>
                                 <th class="text-center"> 020 </th>
                                 <th class="text-center"> 鐘晨芸 </th>
                             </tr>
                             <tr>
-                                <th class="text-center"> 2 </th>
+{{--                                <th class="text-center"> 2 </th>--}}
                                 <th class="text-center"> 050 </th>
                                 <th class="text-center"> 侯安伃 </th>
                                 <th class="text-center"> 161 </th>
                                 <th class="text-center"> 劉以琳 </th>
                             </tr>
                             <tr>
-                                <th class="text-center"> 3 </th>
+{{--                                <th class="text-center"> 3 </th>--}}
                                 <th class="text-center"> 002 </th>
                                 <th class="text-center"> 范予僖 </th>
                                 <th class="text-center"> 026 </th>
                                 <th class="text-center"> 黃淇宣 </th>
                             </tr>
                             <tr>
-                                <th class="text-center"> 4 </th>
+{{--                                <th class="text-center"> 4 </th>--}}
                                 <th class="text-center"> 042 </th>
                                 <th class="text-center"> 周柏崴 </th>
                                 <th class="text-center"> 083 </th>
                                 <th class="text-center"> 周柏諦 </th>
                             </tr>
                             <tr>
-                                <th class="text-center"> 5 </th>
+{{--                                <th class="text-center"> 5 </th>--}}
                                 <th class="text-center"> 085 </th>
                                 <th class="text-center"> 侯鈞諺 </th>
                                 <th class="text-center"> 126 </th>
                                 <th class="text-center"> 陳建廷 </th>
                             </tr>
                             <tr>
-                                <th class="text-center"> 6 </th>
+{{--                                <th class="text-center"> 6 </th>--}}
                                 <th class="text-center"> 084 </th>
                                 <th class="text-center"> 倪詣超 </th>
                                 <th class="text-center"> 092 </th>
                                 <th class="text-center"> 倪采彤 </th>
                             </tr>
                             <tr>
-                                <th class="text-center"> 7 </th>
+{{--                                <th class="text-center"> 7 </th>--}}
                                 <th class="text-center"> 268 </th>
                                 <th class="text-center"> 邱映瑄 </th>
                                 <th class="text-center"> 132 </th>
                                 <th class="text-center"> 邱宇廷 </th>
                             </tr>
                             <tr>
-                                <th class="text-center"> 8 </th>
+{{--                                <th class="text-center"> 8 </th>--}}
                                 <th class="text-center"> 342 </th>
                                 <th class="text-center"> 周祈佑 </th>
                                 <th class="text-center"> 311 </th>
@@ -109,7 +109,7 @@
                     <table class="table table-striped table-bordered table-advance table-hover">
                         <thead>
                         <tr>
-                            <th class="text-center"> 順序 </th>
+{{--                            <th class="text-center"> 順序 </th>--}}
                             <th class="text-center"> 編號 </th>
                             <th class="text-center"> 選手 </th>
                         </tr>
@@ -118,7 +118,7 @@
                         @if ($isView)
                             @foreach ($enrolls as $enroll)
                                 <tr>
-                                    <td class="text-center"> {{ $enroll->appearance }}</td>
+{{--                                    <td class="text-center"> {{ $enroll->appearance }}</td>--}}
                                     <td class="text-center"> {{ $enroll->player_number }}</td>
                                     <td class="text-center"> {{ $enroll->name }}</td>
                                 </tr>
