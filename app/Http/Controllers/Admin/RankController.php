2e7b2c31704897ea680a16a920204a48f6023fa2
jafar <jafartago@gmail.com>
2019-10-27 15:11:23 +0800

    adjust taiwan time

--- app/Http/Controllers/Admin/RankController.php
+++ app/Http/Controllers/Admin/RankController.php
@@ -42,6 +42,7 @@ class RankController extends Controller
 
         $this->processRank($level, $gender, $group, $item, $rankLimit);
 
+
         $gameInfo->open_result_time = now();
         $gameInfo->save();
 //        全國賽不使用以下的判斷
