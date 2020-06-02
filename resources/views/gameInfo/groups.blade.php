c3284e7d1b225fcc10dbbbbfc7fce8979b260d8d
jafar <jafartago@gmail.com>
2020-06-02 21:27:42 +0800

    bdp sme coo mj bi uo

--- resources/views/gameInfo/groups.blade.php
+++ resources/views/gameInfo/groups.blade.php
@@ -9,18 +9,23 @@
     <div class="mh mb-5">
         <div class="container">
             <div class="mt-5 mb-5 text-center">
-                <h2> 分組名冊 </h2>
+                <h2 class="mb-3">分組名冊</h2>
+                <p>點擊積分可展開</p>
+                <p>由於本次參賽人數關係，載入時請耐心等後</p>
             </div>
-            <div class="col-md-12">
+            <div class="col-md-12" mt-3">
                 @foreach($groups as $key => $group)
-                    <table class="table table-striped  table-advance table-hover" style="cursor: pointer;" data-toggle="collapse" href="#{{ $key }}collapse" aria-expanded="false"
-                           aria-controls="{{ $key }}collapse">
+                    <table class="table table-striped  table-advance table-hover" style="cursor: pointer;" data-toggle="collapse" href="#{{ $key }}collapse" aria-expanded="false" aria-controls="{{ $key }}collapse">
+                        <thead class="{{ $key % 2 ? 'thead-dark' : null }}">
                         <tr>
-                            <td>{{ $group->order }} {{ $group->group }} {{ $group->gender }} {{ $group->item }}</td>
+                            <th class="pl-3" style=" border-top-left-radius: 10px;border-bottom-left-radius: 10px;">
+                                {{ $group->order }} {{ $group->group }} {{ $group->gender }} {{ $group->item }}
+                            </th>
+                            <th class="text-right pr-3" style="border-top-right-radius: 10px;border-bottom-right-radius: 10px;">
+                                共 {{ $group->number_of_player }} 人
+                            </th>
                         </tr>
-                        <tr>
-                            <td>共 {{ $group->number_of_player }} 人</td>
-                        </tr>
+                        </thead>
                     </table>
                     <div class="collapse" id="{{ $key }}collapse">
                         <table class="table table-striped table-bordered table-advance table-hover">
@@ -29,7 +34,7 @@
                             <tbody>
                             @foreach ($group->players as $player)
                                 <tr>
-                                    <td class="" style="border:1px solid">
+                                    <td class="" style="">
                                         {{ $player->player_number }} {{ $player->player->name }} ({{ $player->player->city . $player->player->agency }})
                                     </td>
                                 </tr>
