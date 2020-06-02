76669ec90e4bd7a35520b1aaa47f64c3ccb73096
jafar <jafartago@gmail.com>
2020-06-03 00:22:18 +0800

    m

--- resources/views/gameInfo/groups.blade.php
+++ resources/views/gameInfo/groups.blade.php
@@ -13,25 +13,38 @@
                 <p>點擊場次可展開</p>
                 <p>由於本次參賽人數關係，載入時請耐心等後</p>
             </div>
-            <div class="col-md-12" mt-3">
-                @foreach($groups as $key => $group)
-                    <table class="table table-striped  table-advance table-hover" style="cursor: pointer;" data-toggle="collapse" href="#{{ $key }}collapse" aria-expanded="false" aria-controls="{{ $key }}collapse">
-                        <thead class="{{ $key % 2 ? 'thead-dark' : null }}">
-                        <tr>
-                            <th class="pl-3" style=" border-top-left-radius: 10px;border-bottom-left-radius: 10px;">
-                                {{ $group->order }} - {{ $group->group }} {{ $group->gender }} {{ $group->item }}
-                            </th>
-                            <th class="text-right pr-3" style="border-top-right-radius: 10px;border-bottom-right-radius: 10px;">
+            <div class="col-md-12" mt-3
+            ">
+            @foreach($groups as $key => $group)
+                <table class="table table-striped  table-advance table-hover" style="cursor: pointer;" data-toggle="collapse" href="#{{ $key }}collapse" aria-expanded="false"
+                       aria-controls="{{ $key }}collapse">
+                    <thead class="{{ $key % 2 ? 'thead-dark' : null }}">
+                    <tr>
+                        <th class="pl-3" style=" border-top-left-radius: 10px;border-bottom-left-radius: 10px;">
+                            {{ $group->order }} （{{ $group->game_type }}）- {{ $group->group }} {{ $group->gender }} {{ $group->item }}
+                        </th>
+                        <th class="text-right pr-3" style="border-top-right-radius: 10px;border-bottom-right-radius: 10px;">
+                            @if($group->order == '場次30' || $group->order == '場次31' || $group->order == '場次32')
+                                共 0 人
+                            @else
                                 共 {{ $group->number_of_player }} 人
-                            </th>
-                        </tr>
+                            @endif
+                        </th>
+                    </tr>
+                    </thead>
+                </table>
+                <div class="collapse" id="{{ $key }}collapse">
+                    <table class="table table-striped table-bordered table-advance table-hover">
+                        <thead class="thead-inverse">
                         </thead>
-                    </table>
-                    <div class="collapse" id="{{ $key }}collapse">
-                        <table class="table table-striped table-bordered table-advance table-hover">
-                            <thead class="thead-inverse">
-                            </thead>
-                            <tbody>
+                        <tbody>
+                        @if($group->order == '場次30' || $group->order == '場次31' || $group->order == '場次32')
+                            <tr>
+                                <td class="" style="">
+                                    無
+                                </td>
+                            </tr>
+                        @else
                             @foreach ($group->players as $player)
                                 <tr>
                                     <td class="" style="">
@@ -39,14 +52,16 @@
                                     </td>
                                 </tr>
                             @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                    {{--                    <br>--}}
-                @endforeach
+                        @endif
 
-            </div>
+                        </tbody>
+                    </table>
+                </div>
+                {{--                    <br>--}}
+            @endforeach
+
         </div>
+    </div>
     </div>
 @endsection
 
