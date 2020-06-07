dcadb65e7dc16debb011af4ae6d9346d1fcb8839
jafar <jafartago@gmail.com>
2020-06-07 17:12:54 +0800

    ssss

--- resources/views/admin/doc/groups.blade.php
+++ resources/views/admin/doc/groups.blade.php
@@ -20,47 +20,57 @@
                         <table class="table mb-0">
                             <tr>
                                 <td>{{ $group->order }} </td>
-                                <td class="text-center">{{ $group->group }} {{ $group->gender }}子組 {{ $group->item }}</td>
+                                <td class="text-center">({{ $group->game_type }}) {{ $group->group }} {{ $group->gender }}子組 {{ $group->item }}</td>
                                 <td class="text-right">共 {{ $group->number_of_player }} 人</td>
                             </tr>
                         </table>
-                        <table class="table table-bordered m-table m-table--border-tpslalom mb-4">
-                            @php
-                                $i = 0;
-                            @endphp
-                            @while($i < count($group->players))
-                                <tr>
-                                    <td class="w-20">
-                                        @if (isset($group->players[$i]))
-                                            {{ $group->players[$i]->player_number }} {{ $group->players[$i]->name }} ({{ $group->players[$i]->agency }})
-                                        @endif
-                                    </td>
-                                    <td class="w-20">
-                                        @if (isset($group->players[$i + 1]))
-                                            {{ $group->players[$i + 1]->player_number }} {{ $group->players[$i + 1]->name }} ({{ $group->players[$i + 1]->agency }})
-                                        @endif
-                                    </td>
-                                    <td class="w-20">
-                                        @if (isset($group->players[$i + 2]))
-                                            {{ $group->players[$i + 2]->player_number }} {{ $group->players[$i + 2]->name }} ({{ $group->players[$i + 2]->agency }})
-                                        @endif
-                                    </td>
-                                    <td class="w-20">
-                                        @if (isset($group->players[$i + 3]))
-                                            {{ $group->players[$i + 3]->player_number }} {{ $group->players[$i + 3]->name }} ({{ $group->players[$i + 3]->agency }})
-                                        @endif
-                                    </td>
-                                    <td class="w-20">
-                                        @if (isset($group->players[$i + 4]))
-                                            {{ $group->players[$i + 4]->player_number }} {{ $group->players[$i + 4]->name }} ({{ $group->players[$i + 4]->agency }})
-                                        @endif
-                                    </td>
-                                </tr>
+                        @if($group->game_type.$group->group.$group->item == '決賽國中速度過樁菁英組-前溜單足S形(男)' || $group->game_type.$group->group.$group->item == '決賽國中速度過樁菁英組-前溜單足S形(女)' || $group->game_type.$group->group.$group->item == '決賽高中速度過樁菁英組-前溜單足S形(男)')
+                            <table class="table table-bordered m-table m-table--border-tpslalom mb-4">
+                            <tr>
+                                <td class="w-20">
+                                    無（預賽動態取八強）
+                                </td>
+                            </tr>
+                            </table>
+                        @else
+                            <table class="table table-bordered m-table m-table--border-tpslalom mb-4">
                                 @php
-                                    $i += 5
+                                    $i = 0;
                                 @endphp
-                            @endwhile
-                        </table>
+                                @while($i < count($group->players))
+                                    <tr>
+                                        <td class="w-20">
+                                            @if (isset($group->players[$i]))
+                                                {{ $group->players[$i]->player_number }} {{ $group->players[$i]->name }} ({{ $group->players[$i]->agency }})
+                                            @endif
+                                        </td>
+                                        <td class="w-20">
+                                            @if (isset($group->players[$i + 1]))
+                                                {{ $group->players[$i + 1]->player_number }} {{ $group->players[$i + 1]->name }} ({{ $group->players[$i + 1]->agency }})
+                                            @endif
+                                        </td>
+                                        <td class="w-20">
+                                            @if (isset($group->players[$i + 2]))
+                                                {{ $group->players[$i + 2]->player_number }} {{ $group->players[$i + 2]->name }} ({{ $group->players[$i + 2]->agency }})
+                                            @endif
+                                        </td>
+                                        <td class="w-20">
+                                            @if (isset($group->players[$i + 3]))
+                                                {{ $group->players[$i + 3]->player_number }} {{ $group->players[$i + 3]->name }} ({{ $group->players[$i + 3]->agency }})
+                                            @endif
+                                        </td>
+                                        <td class="w-20">
+                                            @if (isset($group->players[$i + 4]))
+                                                {{ $group->players[$i + 4]->player_number }} {{ $group->players[$i + 4]->name }} ({{ $group->players[$i + 4]->agency }})
+                                            @endif
+                                        </td>
+                                    </tr>
+                                    @php
+                                        $i += 5
+                                    @endphp
+                                @endwhile
+                            </table>
+                        @endif
                     @endforeach
                 </div>
             </div>
