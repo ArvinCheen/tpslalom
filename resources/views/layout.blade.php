713da23314475b34f22c327a79a0f9d4951853d7
jafar <jafartago@gmail.com>
2020-07-22 22:08:09 +0800

    v

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -77,9 +77,9 @@
                             <li>
                                 <a href="{{ URL('gameInfo/program') }}"> 線上秩序冊 </a>
                             </li>
-                            <li>
-                                <a href="{{ URL('gameInfo/nationalRecord') }}"> 單足S形全國紀錄 </a>
-                            </li>
+{{--                            <li>--}}
+{{--                                <a href="{{ URL('gameInfo/nationalRecord') }}"> 單足S形全國紀錄 </a>--}}
+{{--                            </li>--}}
 {{--                                    <li>--}}
 {{--                                        <a href="{{ URL('gameInfo/getAppearance') }}"> 出場序名冊 </a>--}}
 {{--                                    </li>--}}
