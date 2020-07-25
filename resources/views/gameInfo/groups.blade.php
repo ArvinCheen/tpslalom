2ce3f7de9929941877210bca862ce7b7885d6fc9
jafar <jafartago@gmail.com>
2020-07-25 16:29:01 +0800

    goo

--- resources/views/gameInfo/groups.blade.php
+++ resources/views/gameInfo/groups.blade.php
@@ -30,39 +30,23 @@
                         </thead>
                     </table>
                     <div class="collapse" id="{{ $key }}collapse">
-                        資料整理中
-{{--                        <table class="table table-striped table-bordered table-advance table-hover">--}}
-{{--                            <thead class="thead-inverse">--}}
-{{--                            </thead>--}}
-{{--                            <tbody>--}}
+                        <table class="table table-striped table-bordered table-advance table-hover">
+                            <thead class="thead-inverse">
+                            </thead>
+                            <tbody>
 
-{{--                            <tr>--}}
-{{--                                <td class="" style="">--}}
-{{--                                    0737 謝牧倫 / 0745 周柏崴--}}
-{{--                                </td>--}}
-{{--                            </tr>--}}
-{{--                            <tr>--}}
-{{--                                <td class="" style="">--}}
-{{--                                    0002 范予僖 / 0449 黃淇宣--}}
-{{--                                </td>--}}
-{{--                            </tr>--}}
-{{--                            <tr>--}}
-{{--                                <td class="" style="">--}}
-{{--                                    0798 邱宇廷 / 0394 邱映瑄--}}
-{{--                                </td>--}}
-{{--                            </tr>--}}
-{{--                            <tr>--}}
-{{--                                <td class="" style="">--}}
-{{--                                    0744 范子聿 / 0797 游瑋筑--}}
-{{--                                </td>--}}
-{{--                            </tr>--}}
-{{--                            <tr>--}}
-{{--                                <td class="" style="">--}}
-{{--                                    0781 侯鈞諺 / 0796 陳建廷--}}
-{{--                                </td>--}}
-{{--                            </tr>--}}
-{{--                            </tbody>--}}
-{{--                        </table>--}}
+                            <tr>
+                                <td class="" style="">
+                                    098 邱映瑄 / 255 邱宇廷
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="" style="">
+                                    001 范予僖 / 123 黃淇宣
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
                     </div>
                 @else
                     <table class="table table-striped  table-advance table-hover" style="cursor: pointer;" data-toggle="collapse" href="#{{ $key }}collapse" aria-expanded="false"
@@ -70,14 +54,14 @@
                         <thead class="{{ $key % 2 ? 'thead-dark' : null }}">
                         <tr>
                             <th class="pl-3" style=" border-top-left-radius: 10px;border-bottom-left-radius: 10px;">
-                                {{ $group->order }} （{{ $group->game_type }}）- {{ $group->group }} {{ $group->item }}
+                                {{ $group->order }} （{{ $group->game_type }}）- {{ $group->group }}{{$group->gender }}子組 {{ $group->item }}
                             </th>
                             <th class="text-right pr-3" style="border-top-right-radius: 10px;border-bottom-right-radius: 10px;">
-                                @if($group->game_type.$group->group.$group->item == '決賽國中速度過樁菁英組-前溜單足S形(男)' || $group->game_type.$group->group.$group->item == '決賽國中速度過樁菁英組-前溜單足S形(女)' || $group->game_type.$group->group.$group->item == '決賽高中速度過樁菁英組-前溜單足S形(男)')
-                                    共 0 人
-                                @else
+{{--                                @if($group->game_type.$group->group.$group->item == '決賽國中速度過樁菁英組-前溜單足S形(男)' || $group->game_type.$group->group.$group->item == '決賽國中速度過樁菁英組-前溜單足S形(女)' || $group->game_type.$group->group.$group->item == '決賽高中速度過樁菁英組-前溜單足S形(男)')--}}
+{{--                                    共 0 人--}}
+{{--                                @else--}}
                                     共 {{ $group->number_of_player }} 人
-                                @endif
+{{--                                @endif--}}
                             </th>
                         </tr>
                         </thead>
@@ -87,21 +71,21 @@
                             <thead class="thead-inverse">
                             </thead>
                             <tbody>
-                            @if($group->game_type.$group->group.$group->item == '決賽國中速度過樁菁英組-前溜單足S形(男)' || $group->game_type.$group->group.$group->item == '決賽國中速度過樁菁英組-前溜單足S形(女)' || $group->game_type.$group->group.$group->item == '決賽高中速度過樁菁英組-前溜單足S形(男)')
-                                <tr>
-                                    <td class="" style="">
-                                        無
-                                    </td>
-                                </tr>
-                            @else
+{{--                            @if($group->game_type.$group->group.$group->item == '決賽國中速度過樁菁英組-前溜單足S形(男)' || $group->game_type.$group->group.$group->item == '決賽國中速度過樁菁英組-前溜單足S形(女)' || $group->game_type.$group->group.$group->item == '決賽高中速度過樁菁英組-前溜單足S形(男)')--}}
+{{--                                <tr>--}}
+{{--                                    <td class="" style="">--}}
+{{--                                        無--}}
+{{--                                    </td>--}}
+{{--                                </tr>--}}
+{{--                            @else--}}
                                 @foreach ($group->players as $player)
                                     <tr>
                                         <td class="" style="">
-                                            {{ $player->player_number }} {{ $player->player->name }} ({{ $player->player->city . $player->player->agency }})
+                                            {{ $player->player_number }} {{ $player->player->name }} ({{ $player->player->city . $player->player->agency_all }})
                                         </td>
                                     </tr>
                                 @endforeach
-                            @endif
+{{--                            @endif--}}
 
                             </tbody>
                         </table>
