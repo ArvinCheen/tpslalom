9ff3e13c1349705f6ec2522888e7d97209bd7f54
jafar <jafartago@gmail.com>
2021-03-24 21:32:09 +0800

    ad tp doc

--- resources/views/paymentInfo/index.blade.php
+++ resources/views/paymentInfo/index.blade.php
@@ -53,7 +53,8 @@
                             @if (env('GAME') == 11)
                                 <div class="col-md-9">
                                     <p>付款資訊：048 王道銀行營業部 0100-0051-3275-88 戶名：曾大宇</p>
-                                    <a style="color:darkblue" href="https://docs.google.com/forms/d/e/1FAIpQLSdt75uY3cwyRBBdfaBSfGnXkcHFetCaMlYdjOpBxyMwTDywvQ/viewform">若完成匯款，請點此連結回報</a>
+                                    <a style="color:darkblue" href="https://docs.google.com/forms/d/e/1FAIpQLSdt75uY3cwyRBBdfaBSfGnXkcHFetCaMlYdjOpBxyMwTDywvQ/viewform">若完成匯款，請點此連結回報</a><br>
+                                    <a href="https://nksds.com/wp-content/uploads/2021/03/%E8%87%BA%E5%8C%97%E5%B8%82110%E5%B9%B4%E7%AC%AC%E4%B8%89%E5%8D%81%E5%85%AB%E5%B1%86%E9%9D%92%E5%B9%B4%E7%9B%83%E6%9A%A8%E5%85%AC%E7%9B%8A%E6%8D%90%E6%AC%BE%E6%B4%BB%E5%8B%95%E6%BA%9C%E5%86%B0%E9%8C%A6%E6%A8%99%E8%B3%BD%E8%87%AA%E7%94%B1%E5%BC%8F%E8%BC%AA%E6%BB%91%E7%AB%B6%E8%B3%BD%E8%A6%8F%E7%AB%A0.pdf" target="_brank">臺北市110年第三十八屆青年盃暨公益捐款活動溜冰錦標賽自由式輪滑競賽規章</a><br>
                                 </div>
                             @endif
 
